a7a577aeb180222e0e85771e5cb101a875b3cf64
Tuna Park <yd041811@naver.com>
2022-05-26 13:56:49 +0900

    22/05/26 3, 4 commit

---
+++ Basic JS/JS3- alert, prompt, confirm.js
@@ -0,0 +1,19 @@
+// 일반 응용
+//const name = prompt("이름을 입력하세요.");
+//alert("환영합니다, " + name + "님");
+
+// 빽틱 응용
+//const name = prompt("너의 이름은?");
+//alert(`알려주셔서 감사합니다. ${name}님. 반갑습니다.`);
+
+// 빽틱 응용(2개 인수 적용)
+//const day = prompt("오늘은 몇일 입니까?", "일");
+//alert(`오늘은 ${day} 입니다! `) 
+
+//confirm 은 확인하는것
+//const clear = confirm("당신은 청소를 했나요?");
+//console.log(clear);
+
+
+//주의사항
+//prompt 로 입력된 것은 모두 " 문 자 형 " 으로 속한다. 숫자X !!!
---
+++ Basic JS/JS4- 형변환-String, Number, Boolean.js
@@ -0,0 +1,7 @@
+// String() 은 문자형으로 변환
+
+// Number() 은 숫자형으로 변환(문자 포함시 NaN을 출력함)
+
+// Boolean() 은 
+// False = 숫자 0, 빈 문자열, null, undefined, Nan
+// 이외는 전부 True
